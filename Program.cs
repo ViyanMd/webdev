3065c6dec23ad468e2f7532f927fc104d5304476
viyan <viyanomd@gmail.com>
2024-01-30 14:18:54 +0100

    context reques/response

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
-app.UseWelcomePage();
+app.Run(HangleRequest);
 
 app.Run();
+
+async Task HangleRequest(HttpContext context)
+{
+    var path = context.Request.Path;
+    var date = DateTime.Now;
+    var response = context.Response;
+
+    if (path == "/date")
+        await response.WriteAsync($"Date: {date.ToShortDateString()}");
+    else if (path == "/time")
+        await response.WriteAsync($"Time: {date.ToShortTimeString()}");
+    else
+        await response.WriteAsync("Not found");
+}   
